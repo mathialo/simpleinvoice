21313cdbcc4c487b3a46ffeb96815722fc253ba7
Mathias Lohne <mathialo@ifi.uio.no>
2018-04-11 19:09:54 +0200

    added texstudio completion file and updated install script

--- install_linux.sh
+++ install_linux.sh
@@ -1,7 +1,21 @@
 #! /bin/bash
+
+if [[ "$EUID" != "0" ]]; then
+	echo "Run me as root!"
+	exit;
+fi
+
 echo "Copying files"
 mkdir -p /usr/local/share/texmf/tex/latex/simpleinvoice
 cp simpleinvoice.sty /usr/local/share/texmf/tex/latex/simpleinvoice
 echo "Updating package index"
 cd /usr/local/share/texmf/ && mktexlsr > /dev/null
+
+read -p "Are you using TeXstudio? [y/n] " yn
+if [[ "$yn" == "y" || "$yn" == "Y" ]]; then
+	echo "Installing auto-completion file"
+	cp simpleinvoice.cwl ~/.config/texstudio/completion/user
+	chmod 644 ~/.config/texstudio/completion/user/simpleinvoice.cwl
+fi
+
 echo "Installation done"
